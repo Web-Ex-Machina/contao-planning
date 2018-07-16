6bd17109b0e687a189d7cbe7135b5fb3d19e55fe
Julien Thirion <julien@webexmachina.fr>
2018-07-16 08:56:27 +0200

    + Use CDN when it's possible (see what can we do for this) + Add icalendar dependancy

--- library/WEM/Planning/Module/DisplayPlanning.php
+++ library/WEM/Planning/Module/DisplayPlanning.php
@@ -67,17 +67,17 @@ class DisplayPlanning extends ModulePlanning
 		$GLOBALS['TL_HEAD'][] = '
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 			<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-			<link rel="stylesheet" href="system/modules/wem-contao-planning/assets/vendor/fullcalendar/3.8.2/fullcalendar.min.css">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css">
 			<link rel="stylesheet" href="system/modules/wem-contao-planning/assets/vendor/icalendar/jquery.icalendar.css">
-			<script src="system/modules/wem-contao-planning/assets/vendor/fullcalendar/3.8.2/lib/jquery.min.js"></script>
-			<script src="system/modules/wem-contao-planning/assets/vendor/fullcalendar/3.8.2/lib/jquery-ui.min.js"></script>
-			<script src="system/modules/wem-contao-planning/assets/vendor/fullcalendar/3.8.2/lib/moment.min.js"></script>
+			<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 			<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-			<script src="system/modules/wem-contao-planning/assets/vendor/fullcalendar/3.8.2/fullcalendar.min.js"></script>
-			<script src="system/modules/wem-contao-planning/assets/vendor/fullcalendar/3.8.2/locale/fr.js"></script>
-			<script src="system/modules/wem-contao-planning/assets/vendor/icalendar/jquery.icalendar.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/locale/fr.js"></script>
+			<script src="system/modules/wem-contao-planning/assets/vendor/icalendar/jquery.icalendar.min.js"></script>
 			<script src="system/modules/wem-contao-planning/assets/js/calendar_functions.js"></script>
 		';
 
